9ca299ece5b73b8bf12b740d957f077c920ae0a4
Wandberg de Lima <wandberg@gmail.com>
2022-09-04 19:33:38 -0300

    tratando atributo e referencia do metod transferir

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -2,10 +2,12 @@
 export class ContaCorrente {
     agencia;
     // atributo privado porem ainda não esta valido para produção (#saldo) o padrão é utilizar _saldo , porem não torna o atributo devidamente privado atualmente no js
+    // operações (parametros ou argumentos) e metodos dentro da classe
     // #saldo = 0;
     _saldo = 0;
-    // operações (parametros ou argumentos) e metodos dentro da classe
-  
+    // asociação de cliente a conta corrente do mesmo (iniciar o atributo cliente)
+    cliente;
+    
     // operação de SAQUE - inicio da Branch SAQUE
     sacar(valor) {
       if (this._saldo >= valor) {
@@ -21,4 +23,14 @@ export class ContaCorrente {
       }
       this._saldo += valor;
     }
+
+    // Operação de Transferência de valores entre contas
+    // Ter cuidade e não adicionar atributos dinamicamente a metodos para consertar classes, pois assim pode-se criar um erro na alteração do valor do atributo referenciado.
+    transferir(valor, conta){
+      // exemplo: const valorSacado = this.sacar(valor = 20);
+      // conta.cidade = "Rio de Janeiro";
+      const valorSacado = this.sacar(valor);
+      conta.depositar(valorSacado);
+    }
+
   }
--- index.js
+++ index.js
@@ -5,14 +5,24 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+// Retorno do atributo cliente para vincular a conta corrente a quem pertence 
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+contaCorrenteRicardo.transferir(200 , conta2);
 
+console.log('====================================');
 console.log(contaCorrenteRicardo);
+console.log('====================================');
+console.log(conta2);
+console.log('====================================');
