fd610d74e305833829335082e8c9b7bc4edee2fa
Wandberg de Lima <wandberg@gmail.com>
2022-09-04 15:33:01 -0300

    utilizando o campo _saldo

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ class ContaCorrente {
     agencia;
 
     // atributo privado porem ainda não esta valido para produção (#saldo) o padrão é utilizar _saldo , porem não torna o atributo devidamente privado atualmente no js
-    #saldo = 0;
+    // #saldo = 0;
     
+    _saldo = 0;
     // operações (parametros ou argumentos) e metodos dentro da classe
     
     // operação de SAQUE - inicio da Branch SAQUE
     sacar(valor){
-        if (this.#saldo >= valor) {
-            this.#saldo -= valor;
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
         }
         
     }
@@ -23,8 +24,7 @@ class ContaCorrente {
     // Operação de Deposito - inicio da Branch DEPOSITO
     depositar(valor){
         if (valor > 0) {
-            this.#saldo += valor;
-        }
+            this._saldo += valor;
     }
 
 }
